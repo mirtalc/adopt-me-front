c5437a758a01ffc5f042d360f34e761dd6029f7d
mirta.lopez <mirta.lopez@soprasteria.com>
2020-10-25 10:42:06 +0100

    feat(axios): set-up basic axios and make example call for animals

--- src/views/Animals.vue
+++ src/views/Animals.vue
@@ -1,13 +1,54 @@
 <template>
   <div class="animals">
     <h1>>> Animals component, still WIP.</h1>
+    <div class="container">
+      <div v-for="animal in APIData" :key="animal.id" class="card">
+        <div>Name: {{ animal.name }}</div>
+        <div>Adoption status: {{ animal.status }}</div>
+        <div>Animal type: (pending)</div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import { getAPI } from "../axios-api";
 export default {
   name: "Animals",
+  data() {
+    return {
+      APIData: [],
+    };
+  },
+  created() {
+    getAPI
+      .get("/animals/")
+      .then((response) => {
+        console.log("Axios has received data: ", response.data);
+        this.APIData = response.data;
+      })
+      .catch((error) => {
+        console.warn("Error trying to receive data: ", error);
+      });
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  max-width: 1200px;
+  margin: auto;
+  border: 1px dashed gray;
+  background-color: #f9dcc4;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+.card {
+  background-color: #cbc0d3;
+  border: 1px solid gray;
+  margin: 1rem;
+  padding: 1rem;
+}
+</style>
