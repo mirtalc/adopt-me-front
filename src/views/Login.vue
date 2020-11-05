7af2e058cbfcf80f38ed1a2b7894821d106f589b
mirtalc <mirta.lopez.cardona@gmail.com>
2020-11-05 12:21:24 +0100

    feat(login): add checks for input values

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,80 +1,95 @@
 <template>
-  <div
-    class="sm:max-w-xl sm:mx-auto sm:my-16 bg-indigo-300 border rounded-lg p-8 text-gray-800"
-  >
-    <p class="pl-6">
-      Welcome! <br />
-      Please, enter your credentials.
-    </p>
+  <div class="squared-container sm:max-w-xl sm:mx-auto my-8 sm:my-16">
+    <p>Welcome back! <span class="ml-4">\(ᵔᵕᵔ)/</span></p>
+    <p>Please, enter your credentials.</p>
 
-    <form v-on:submit.prevent="login" class="m-auto text-center mt-8">
+    <form @submit.prevent="checkAndLogin" class="centered-container">
       <div class="field">
         <label for="username">Username</label>
         <input type="text" name="username" id="username" v-model="username" />
       </div>
       <div class="field">
         <label for="password">Password</label>
-        <input
-          type="password"
-          name="password"
-          id="password"
-          v-model="password"
-        />
+        <input type="password" name="password" id="password" v-model="password" />
       </div>
-      <div>
-        <button
-          type="submit"
-          class="bg-indigo-400 hover:bg-indigo-500 py-4 w-full mt-8 text-xl rounded-lg"
-        >
-          Log me in!
-        </button>
+      <button type="submit" class="big-button hover:bg-indigo-500">
+        Log me in!
+      </button>
+      <div v-show="shouldShowErrors" class="error-message mt-4">
+        <ul>
+          <li v-for="inputError in inputErrors" :key="inputError.id">— {{ inputError }}</li>
+        </ul>
+        <p v-show="loginError">{{ loginError }}</p>
+      </div>
+      <div class="mt-20 secondary">
+        Still not a member? You can register
+        <router-link :to="{ name: routesInfo.register.name }">
+          <span class="underline font-bold">here</span>
+        </router-link>
       </div>
     </form>
-    <p v-if="incorrectCredentials" class="text-red-500">
-      Error logging in. Please, check your user and password!
-    </p>
   </div>
 </template>
 
 <script>
+import { routesInfo } from "../constants/routesInfo"
+
 export default {
   name: "Login",
   data() {
     return {
       username: "",
       password: "",
-      incorrectCredentials: false,
-    };
+      inputErrors: [],
+      loginError: null,
+      routesInfo,
+    }
+  },
+  computed: {
+    hasInputErrors: function() {
+      return this.inputErrors.length > 0
+    },
+    shouldShowErrors: function() {
+      return this.hasInputErrors || this.loginError
+    },
   },
   methods: {
+    checkAndLogin() {
+      this.checkValues()
+      if (!this.hasInputErrors) this.login()
+    },
+    checkValues() {
+      this.inputErrors = []
+      if (!this.username) this.inputErrors.push("Username field may not be empty")
+      if (!this.password) this.inputErrors.push("Password field may not be empty")
+    },
     login() {
       this.$store
         .dispatch("userLogin", {
           username: this.username,
           password: this.password,
         })
-        .then(() => {
-          this.$router.push({ name: "animals" });
-        })
-        .catch((error) => {
-          console.warn(error);
-          this.incorrectCredentials = true;
-        });
+        .then(() => this.processLoginOk())
+        .catch((error) => this.processLoginError(error))
+    },
+    processLoginOk() {
+      this.$router.push({ name: routesInfo.animals.name })
+    },
+    processLoginError(error) {
+      let error_code = error.response.data.error_code
+      if (error_code == "no_active_account") {
+        this.loginError = "Error: Incorrect user or password"
+      } else {
+        this.loginError = "Unknown error; please try again later. More info at Console."
+      }
+      console.warn("Error while logging in: ", error)
     },
   },
-};
+}
 </script>
 
 <style scoped lang="postcss">
 .field {
   @apply p-4 inline-block;
-}
-
-label {
-  @apply min-w-32 inline-block;
-}
-
-input {
-  @apply w-48 rounded bg-blue-100;
 }
 </style>
